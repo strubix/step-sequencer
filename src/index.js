5e0cb44fe96115bc27ab378cac5a728fa95b6fd1
Lucas <lucas.strubi@gmail.com>
2017-01-19 10:40:14 +0100

    refactor index

--- src/index.js
+++ src/index.js
@@ -1,21 +1,2 @@
-import Pattern from "./pattern";
-
-const bpm = 100;
-const stage = new createjs.Stage("canvasElementId");
-
-const kick = new Pattern({stage: stage});
-
-$('.playSound').on('click', function() {
-  let beat = 0;
-  kick.sound.loop = setInterval(() => {
-    kick.displayBeats(beat);
-    if(kick.beat[beat].read){
-      kick.sound.playSound();
-    }
-    beat == 8 ? beat = 0 : beat++;
-  }, 60000 / bpm);
-});
-
-$('.stopSound').on('click', function() {
-  clearInterval(sound.loop);
-});
+import Stage from "./stage";
+const test = new Stage();
